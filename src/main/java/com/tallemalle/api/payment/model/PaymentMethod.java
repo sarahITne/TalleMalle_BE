6099cc027bcfa79e6ea015fa63a5f4ed75667308
shinukang <shinu9643@gmail.com>
2026-01-29 18:24:45 +0900

    [Refactor] 결제 DTO 수정 - 결제 수단 등록 DTO 메서드 추가 - 결제 수단 DTO 메서드 추가

--- src/main/java/com/tallemalle/api/payment/model/PaymentMethod.java
+++ src/main/java/com/tallemalle/api/payment/model/PaymentMethod.java
@@ -3,4 +3,25 @@ package com.tallemalle.api.payment.model;
 public class PaymentMethod {
     private Integer id;
     private String alias;
+
+    public PaymentMethod(Integer id, String alias) {
+        this.id = id;
+        this.alias = alias;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 }
--- src/main/java/com/tallemalle/api/payment/model/PaymentMethodEnroll.java
+++ src/main/java/com/tallemalle/api/payment/model/PaymentMethodEnroll.java
@@ -8,9 +8,69 @@ public class PaymentMethodEnroll {
         private String alias;
         private String billingKey;
         private Boolean asDefault;
+
+        public Request(String userId, String alias, String billingKey, Boolean asDefault) {
+            this.userId = userId;
+            this.alias = alias;
+            this.billingKey = billingKey;
+            this.asDefault = asDefault;
+        }
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+
+        public String getAlias() {
+            return alias;
+        }
+
+        public void setAlias(String alias) {
+            this.alias = alias;
+        }
+
+        public String getBillingKey() {
+            return billingKey;
+        }
+
+        public void setBillingKey(String billingKey) {
+            this.billingKey = billingKey;
+        }
+
+        public Boolean getAsDefault() {
+            return asDefault;
+        }
+
+        public void setAsDefault(Boolean asDefault) {
+            this.asDefault = asDefault;
+        }
     }
     public static class Response {
         private Integer defaultMethodId;
         private List<PaymentMethod> paymentMethods;
+
+        public Response(Integer defaultMethodId, List<PaymentMethod> paymentMethods) {
+            this.defaultMethodId = defaultMethodId;
+            this.paymentMethods = paymentMethods;
+        }
+
+        public Integer getDefaultMethodId() {
+            return defaultMethodId;
+        }
+
+        public void setDefaultMethodId(Integer defaultMethodId) {
+            this.defaultMethodId = defaultMethodId;
+        }
+
+        public List<PaymentMethod> getPaymentMethods() {
+            return paymentMethods;
+        }
+
+        public void setPaymentMethods(List<PaymentMethod> paymentMethods) {
+            this.paymentMethods = paymentMethods;
+        }
     }
 }
