2aab4a966a90824ed980a5f3bfb29cd6c853423b
saralove20 <saralove20@naver.com>
2026-01-30 17:33:44 +0900

    [Feat] 공지사항 목록 조회 dto 생성

--- src/main/java/com/tallemalle/api/notice/model/NoticeDto.java
+++ src/main/java/com/tallemalle/api/notice/model/NoticeDto.java
@@ -12,42 +12,44 @@ public class NoticeDto {
 //    private LocalDateTime createdAt;    // 생성일시
 //    private LocalDateTime updatedAt;    // 수정일시
 
-        // 공지사항 리스트 조회 - 응답
-        public static class NoticeRes {
-            private Long noticeIdx;             // 공지사항 idx (PK)
-            private String title;               // 제목
-            private String tag;                 // 태그
-            private Boolean isPinned;           // 필독 여부
-            private LocalDateTime createdAt;    // 생성일시
+    // 3. DTO 생성
+    // 공지사항 리스트 조회 - 응답
+    public static class NoticeRes {
+        private Long noticeIdx;             // 공지사항 idx (PK)
+        private String title;               // 제목
+        private String tag;                 // 태그
+        private Boolean isPinned;           // 필독 여부
+        private LocalDateTime createdAt;    // 생성일시
 
+        public NoticeRes() {
+        }
 
+        public NoticeRes(Long noticeIdx, String title, String tag, Boolean isPinned, LocalDateTime createdAt) {
+            this.noticeIdx = noticeIdx;
+            this.title = title;
+            this.tag = tag;
+            this.isPinned = isPinned;
+            this.createdAt = createdAt;
+        }
 
-            public NoticeRes(Long noticeIdx, String title, String tag, Boolean isPinned, LocalDateTime createdAt) {
-                this.noticeIdx = noticeIdx;
-                this.title = title;
-                this.tag = tag;
-                this.isPinned = isPinned;
-                this.createdAt = createdAt;
-            }
+        public Long getNoticeIdx() {
+            return noticeIdx;
+        }
 
-            public Long getNoticeIdx() {
-                return noticeIdx;
-            }
+        public String getTitle() {
+            return title;
+        }
 
-            public String getTitle() {
-                return title;
-            }
+        public String getTag() {
+            return tag;
+        }
 
-            public String getTag() {
-                return tag;
-            }
+        public Boolean getPinned() {
+            return isPinned;
+        }
 
-            public Boolean getPinned() {
-                return isPinned;
-            }
-
-            public LocalDateTime getCreatedAt() {
-                return createdAt;
+        public LocalDateTime getCreatedAt() {
+            return createdAt;
         }
     }
 
