c367d174b74328510c020058cc603a5e3581113c
saralove20 <saralove20@naver.com>
2026-02-01 02:34:19 +0900

    [Feat] 로그인 기능 구현

--- src/main/java/com/tallemalle/api/user/UserController.java
+++ src/main/java/com/tallemalle/api/user/UserController.java
@@ -4,14 +4,16 @@ import com.tallemalle.api.common.BaseResponse;
 import com.tallemalle.api.common.Controller;
 import com.tallemalle.api.user.model.UserDto;
 import com.tallemalle.api.utils.JsonParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.crypto.SecretKey;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UserController implements Controller {
     DataSource ds;
@@ -54,7 +56,44 @@ public class UserController implements Controller {
             }
 
         } else if (req.getRequestURI().contains("login") && req.getMethod().equals("POST")) {
-            System.out.println("login code");
+            UserDto.LoginReq dto = JsonParser.from(req, UserDto.LoginReq.class);
+
+            String sql = "SELECT * FROM users WHERE email=? AND password=?";
+
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+
+                try (Connection conn = ds.getConnection()) {
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    pstmt.setString(1, dto.getEmail());
+                    pstmt.setString(2, dto.getPassword());
+
+                    ResultSet rs = pstmt.executeQuery();
+                    if (rs.next()) {
+                        // 토큰 생성해서 응답에 토큰을 넣어줘야 한다.
+                        String key = "djkflajdklajf9032uj9042fjfoijsadkj0924jtfiorejoij90f429eodji";
+                        SecretKey encodedKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(key));
+
+                        // compact는 builder()라는 스태틱 메서드가 반환값으로 반환해준 것에 들어있는 메서드, 그러니까 builder는 아마도 클래스를 반환해주겠지요
+                        // Jwts.builder().compact();
+
+                        String jwt = Jwts.builder()
+                                .claim("userId",rs.getInt("user_id"))
+                                .claim("email",rs.getString("email"))
+                                .issuedAt(new Date())
+                                .expiration(new Date(System.currentTimeMillis() + 30000))
+                                .signWith(encodedKey)
+                                .compact();
+
+                        resp.setHeader("Set-Cookie", "ATOKEN="+jwt + "; Path=/");
+
+                        System.out.println(jwt);
+                        return BaseResponse.success("로그인 성공");
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
         return null;
     }
--- src/main/java/com/tallemalle/api/user/model/UserDto.java
+++ src/main/java/com/tallemalle/api/user/model/UserDto.java
@@ -73,9 +73,63 @@ public class UserDto {
     }
 
     // 로그인 - 요청 dto
+    public static class LoginReq {
+        private String email;
+        private String password;
 
+        public LoginReq() {
+        }
+
+        public LoginReq(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 
     // 로그인 - 응답 dto
+    public static class LoginRes {
+        private Long userId;
+        private String email;
 
+        public LoginRes() {
+        }
+
+        public LoginRes(Long userId, String email) {
+            this.userId = userId;
+            this.email = email;
+        }
+
+        public Long getUserId() {
+            return userId;
+        }
+
+        public void setUserId(Long userId) {
+            this.userId = userId;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
 
 }
