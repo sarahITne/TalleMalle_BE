4f72b854873e431d21a2fa02ed0f513de69b5c5b
saralove20 <saralove20@naver.com>
2026-01-31 22:09:49 +0900

    [Feat] 공지사항 상세 조회 시 조회 수 증가 기능 구현

--- src/main/java/com/tallemalle/api/notice/NoticeRepository.java
+++ src/main/java/com/tallemalle/api/notice/NoticeRepository.java
@@ -1,6 +1,5 @@
 package com.tallemalle.api.notice;
 
-import com.tallemalle.api.common.BaseResponse;
 import com.tallemalle.api.notice.model.NoticeDto;
 
 import javax.sql.DataSource;
@@ -19,63 +18,77 @@ public class NoticeRepository {
     }
 
     // 5. DB에 CRUD 실행
+    // 공지사항 목록 조회
     public List<NoticeDto.NoticeRes> findAll() {
         String sql = "SELECT * FROM notice";
 
         try {
             Class.forName("org.mariadb.jdbc.Driver");
 
-            // DB 연결 객체를 다 사용하고 나면 반납할 수 있도록 수정
-            try (Connection conn = ds.getConnection()) {
+        // DB 연결 객체를 다 사용하고 나면 반납할 수 있도록 수정
+        try (
+                Connection conn = ds.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql);
-                ResultSet rs = pstmt.executeQuery();    // DB에서 가져온 결과표 저장
+                ResultSet rs = pstmt.executeQuery()    // DB에서 가져온 결과표 저장
+        ) {
+            List<NoticeDto.NoticeRes> list = new ArrayList<>();
 
-                List<NoticeDto.NoticeRes> list = new ArrayList<>();
+            while (rs.next()) {         // 다음 행으로 이동, 처음에는 커서가 아무것도 가리키지 않았는데 이거 실행하자 마자 바로 1번행으로 이동 (이동 가능하면 : true, 다음 행 없어서 이동 못했으면 : false)
+                NoticeDto.NoticeRes returnDto = new NoticeDto.NoticeRes(rs.getLong("notice_idx"), rs.getString("title"), rs.getString("tag"), rs.getBoolean("is_pinned"), rs.getTimestamp("created_at").toLocalDateTime()
 
-                while (rs.next()) {         // 다음 행으로 이동, 처음에는 커서가 아무것도 가리키지 않았는데 이거 실행하자 마자 바로 1번행으로 이동 (이동 가능하면 : true, 다음 행 없어서 이동 못했으면 : false)
-                    NoticeDto.NoticeRes returnDto = new NoticeDto.NoticeRes(
-                            rs.getLong("notice_idx"),
-                            rs.getString("title"),
-                            rs.getString("tag"),
-                            rs.getBoolean("is_pinned"),
-                            rs.getTimestamp("created_at").toLocalDateTime()
-
-                    );
-                    list.add(returnDto);
-                }
-                return list;
+                );
+                list.add(returnDto);
+            }
+            return list;
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public NoticeDto.NoticeDetailRes findbyId(Long noticeIdx) {
+    // 공지사항 상세 조회
+    public NoticeDto.NoticeDetailRes findById(Long noticeIdx) {
         String sql = "SELECT * FROM notice WHERE notice_idx = ?";
 
         try {
             Class.forName("org.mariadb.jdbc.Driver");
 
             // DB 연결 객체를 다 사용하고 나면 반납할 수 있도록 수정
-            try (Connection conn = ds.getConnection()) {
-                PreparedStatement pstmt = conn.prepareStatement(sql);
+            try (
+                    Connection conn = ds.getConnection();
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+            ) {
+
                 pstmt.setLong(1, noticeIdx);
                 ResultSet rs = pstmt.executeQuery();    // DB에서 가져온 결과표 저장
 
                 if (rs.next()) {         // 다음 행으로 이동, 처음에는 커서가 아무것도 가리키지 않았는데 이거 실행하자 마자 바로 1번행으로 이동 (이동 가능하면 : true, 다음 행 없어서 이동 못했으면 : false)
-                    return new NoticeDto.NoticeDetailRes(
-                            rs.getLong("notice_idx"),
-                            rs.getString("title"),
-                            rs.getString("contents"),
-                            rs.getString("tag"),
-                            rs.getBoolean("is_pinned"),
-                            rs.getTimestamp("created_at").toLocalDateTime()
-                    );
+                    return new NoticeDto.NoticeDetailRes(rs.getLong("notice_idx"), rs.getString("title"), rs.getString("contents"), rs.getString("tag"), rs.getBoolean("is_pinned"), rs.getTimestamp("created_at").toLocalDateTime());
                 }
                 return null;
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // 조회수 증가
+    public void increaseViews(Long noticeIdx) {
+        String sql = "UPDATE notice SET views = views + 1 WHERE notice_idx = ?";
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            try (
+                 Connection conn = ds.getConnection();
+                 PreparedStatement pstmt = conn.prepareStatement(sql);
+            ) {
+                pstmt.setLong(1, noticeIdx);
+                pstmt.executeQuery();    // DB에서 가져온 결과표 저장
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
