0f2d798acca873d3153f3f6dbfd7430ec4739ed7
Beomsoo <linoleca@naver.com>
2026-03-12 16:38:01 +0900

    [Feat] 웹 소켓 서버 STOMP 사용하도록 변경

--- src/main/java/org/example/tallemalle_backend/config/WebSocketConfig.java
+++ src/main/java/org/example/tallemalle_backend/config/WebSocketConfig.java
@@ -4,21 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.example.tallemalle_backend.config.interceptor.JwtHandshakeInterceptor;
 import org.example.tallemalle_backend.config.websocket.WebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final WebSocketHandler webSocketHandler;
     private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws")
-                .setAllowedOrigins("*")
-                .addInterceptors(jwtHandshakeInterceptor);
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .addInterceptors(jwtHandshakeInterceptor)
+                .setAllowedOrigins("*");
+    }
+
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // 구독자가 메시지를 받을 경로의 시작 부분
+        registry.setApplicationDestinationPrefixes("/app"); // 클라이언트가 메시지를 보낼 때 사용할 주소의 시작 부분
+        registry.setUserDestinationPrefix("/user"); // 특정 사용자에게 메시지를 보낼 때 사용할 주소의 시작 부분
     }
 }
