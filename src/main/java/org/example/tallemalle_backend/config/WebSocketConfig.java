9a77c79320778468adbd071f045083df80804c24
jdh <wjdehd555@gmail.com>
2026-03-13 22:57:52 +0900

    [Feat] 웹소켓 통신 기능 기본 구조 추가

--- src/main/java/org/example/tallemalle_backend/config/WebSocketConfig.java
+++ src/main/java/org/example/tallemalle_backend/config/WebSocketConfig.java
@@ -13,8 +13,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
+//                .withSockJS();
     }
 
     @Override
