cc4a8d7be7ec171e32002fe770cf2d746cac5aba
Beomsoo <linoleca@naver.com>
2026-03-15 00:22:03 +0900

    [Feat] 참여 멤버 조회 API 추가

---
+++ src/main/java/org/example/tallemalle_backend/participation/ParticipationController.java
@@ -0,0 +1,28 @@
+package org.example.tallemalle_backend.participation;
+
+import lombok.RequiredArgsConstructor;
+import org.example.tallemalle_backend.common.model.BaseResponse;
+import org.example.tallemalle_backend.participation.model.Participation;
+import org.example.tallemalle_backend.participation.model.ParticipationDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recruits")
+@RequiredArgsConstructor
+public class ParticipationController {
+    private final ParticipationRepository participationRepository;
+
+    @GetMapping("/{recruitId}/participants")
+    public ResponseEntity participants(@PathVariable Long recruitId) {
+        List<ParticipationDto.MemberRes> members = participationRepository.findAllByRecruit_Id(recruitId)
+                .stream()
+                .map(Participation::getUser)
+                .map(ParticipationDto.MemberRes::from)
+                .toList();
+
+        return ResponseEntity.ok(BaseResponse.success(members));
+    }
+}
