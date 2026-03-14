68a80419895f2ef6e0e3dfe9cb7deedc83366498
saralove20 <saralove20@naver.com>
2026-03-14 18:32:04 +0900

    [Feat] 회원 가입시 전송한 메일 내역 저장하도록 구현 - 메일마다 고유한 uuid 생성 - 어떤 이메일에 어떤 uuid로 인증 메일을 보냈는지 DB에 저장

--- src/main/java/org/example/tallemalle_backend/user/UserService.java
+++ src/main/java/org/example/tallemalle_backend/user/UserService.java
@@ -3,6 +3,7 @@ package org.example.tallemalle_backend.user;
 import lombok.RequiredArgsConstructor;
 import org.example.tallemalle_backend.common.exception.BaseException;
 import org.example.tallemalle_backend.user.model.AuthUserDetails;
+import org.example.tallemalle_backend.user.model.EmailVerify;
 import org.example.tallemalle_backend.user.model.User;
 import org.example.tallemalle_backend.user.model.UserDto;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 import static org.example.tallemalle_backend.common.model.BaseResponseStatus.SIGNUP_DUPLICATE_EMAIL;
 
@@ -19,6 +22,7 @@ public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final EmailService emailService;
+    private final EmailVerifyRepository emailVerifyRepository;
 
     // 회원가입
     public UserDto.SignupRes signup(UserDto.SignupReq dto) {
@@ -36,7 +40,17 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);  // 저장 후 user에 idx 세팅됨
 
         // 4. 이메일 인증 메일 보내기
-        emailService.sendWelcomeMail(dto.getEmail());
+        String uuid = UUID.randomUUID().toString();     // 이메일 인증에 사용할 고유 토큰 (UUID) 생성
+        emailService.sendWelcomeMail(uuid, dto.getEmail());
+
+        // 4-1. 이메일 전송 내역 저장
+        EmailVerify emailVerify =
+                EmailVerify.builder()
+                        .email(dto.getEmail())
+                        .uuid(uuid)
+                        .build();
+
+        emailVerifyRepository.save(emailVerify);
 
         return UserDto.SignupRes.from(user);
     }
