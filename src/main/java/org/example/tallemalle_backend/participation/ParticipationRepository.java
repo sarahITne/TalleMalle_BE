6487781363f07eb39eea63768dc463c613ac462a
Beomsoo <linoleca@naver.com>
2026-03-17 23:38:10 +0900

    [Refactor] findByUserIdxAndRecruitIdx 메서드 통일 - ParticipationRepository 동일 기능 중복 함수 통일

--- src/main/java/org/example/tallemalle_backend/participation/ParticipationRepository.java
+++ src/main/java/org/example/tallemalle_backend/participation/ParticipationRepository.java
@@ -5,11 +5,12 @@ import org.example.tallemalle_backend.recruit.model.Recruit;
 import org.example.tallemalle_backend.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
     boolean existsByUserAndRecruit(User user, Recruit recruit);
     Optional<Participation> findByUserIdxAndRecruitIdx(Long UserIdx, Long recruitIdx);
-    java.util.List<Participation> findAllByRecruit_Idx(Long recruitId);
-    java.util.List<Participation> findAllByUser_Idx(Long userIdx);
+    List<Participation> findAllByRecruit_Idx(Long recruitId);
+    List<Participation> findAllByUser_Idx(Long userIdx);
 }
