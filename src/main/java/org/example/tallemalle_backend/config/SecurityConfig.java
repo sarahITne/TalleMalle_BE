1198f77ef2c0a1bed66111f7cd8e15ef51b878ae
saralove20 <saralove20@naver.com>
2026-03-20 01:32:38 +0900

    [Fix] SicurityConfig api 권한 설정 수정
    - /user/verify-identity

--- src/main/java/org/example/tallemalle_backend/config/SecurityConfig.java
+++ src/main/java/org/example/tallemalle_backend/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
                         // 접근 제어
                         .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()    // OPTIONS 요청은 무조건 허용 (CORS Preflight 해결)
                         .requestMatchers("/user/signup/extra").authenticated()
-                        .requestMatchers("/user/signup", "/user/login", "/user/verify",
+                        .requestMatchers("/user/signup", "/user/login", "/user/verify", "/user/verify-identity",
                                 "/user/signup/check-email", "/user/signup/check-nickname").permitAll()
                         .requestMatchers("/notices/**").authenticated()
                         .requestMatchers("/driver/login", "/driver/signup").permitAll()
