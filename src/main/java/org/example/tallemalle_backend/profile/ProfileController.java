fa086fa4b8bc8c57cd91085a1c031d49c738fd49
shinukang <shinu9643@gmail.com>
2026-03-19 12:49:14 +0900

    [feat] 프로필 사진 업로드 기능 추가

--- src/main/java/org/example/tallemalle_backend/profile/ProfileController.java
+++ src/main/java/org/example/tallemalle_backend/profile/ProfileController.java
@@ -1,10 +1,10 @@
 package org.example.tallemalle_backend.profile;
 
 import lombok.RequiredArgsConstructor;
-import org.example.tallemalle_backend.common.exception.BaseException;
 import org.example.tallemalle_backend.common.model.BaseResponse;
-import org.example.tallemalle_backend.common.model.BaseResponseStatus;
 import org.example.tallemalle_backend.profile.data.dto.ProfileDto;
+import org.example.tallemalle_backend.upload.PresignedUploadDto;
+import org.example.tallemalle_backend.upload.UploadService;
 import org.example.tallemalle_backend.user.model.AuthUserDetails;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ProfileController {
     private final ProfileService profileService;
+    private final UploadService uploadService;
 
     @GetMapping("/read")
     public ResponseEntity read(@AuthenticationPrincipal AuthUserDetails user) {
@@ -25,5 +26,11 @@ public class ProfileController {
     public ResponseEntity update(@AuthenticationPrincipal AuthUserDetails user,
                                  @RequestBody ProfileDto.UpdateRequest dto) {
         return ResponseEntity.ok(BaseResponse.success(profileService.update(user, dto)));
+    }
+
+    @PostMapping("/image/presign")
+    public ResponseEntity presign(@RequestBody PresignedUploadDto.PresignReq req) {
+        PresignedUploadDto.PresignRes result = uploadService.presign(req);
+        return ResponseEntity.ok(BaseResponse.success(result));
     }
 }
